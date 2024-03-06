3476b854649bfb2189a258a1c78887cd28b7eb1f
jagodagardzielewska <122111893+jagodagardzielewska@users.noreply.github.com>
2024-03-06 21:39:57 +0100

    player main view

---
+++ app/src/main/java/com/example/proindividual/PlayerMain.java
@@ -0,0 +1,13 @@
+package com.example.proindividual;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_main);
+    }
+}

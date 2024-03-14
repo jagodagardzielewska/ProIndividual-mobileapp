a8462b8c808e615c05f255894ebb6ffe11be67a8
jagodagardzielewska <122111893+jagodagardzielewska@users.noreply.github.com>
2024-03-14 23:30:32 +0100

    training rating

---
+++ app/src/main/java/com/example/proindividual/TrainingEnding.java
@@ -0,0 +1,14 @@
+package com.example.proindividual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainingEnding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_ending);
+    }
+}

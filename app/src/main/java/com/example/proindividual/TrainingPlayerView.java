186c83a8de34ce39072c410be4c1553fe54e8644
jagodagardzielewska <122111893+jagodagardzielewska@users.noreply.github.com>
2024-03-14 22:40:30 +0100

    training player view

---
+++ app/src/main/java/com/example/proindividual/TrainingPlayerView.java
@@ -0,0 +1,14 @@
+package com.example.proindividual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainingPlayerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_player_view);
+    }
+}

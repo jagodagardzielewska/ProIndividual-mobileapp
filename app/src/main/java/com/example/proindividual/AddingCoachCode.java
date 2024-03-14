6fb55f60501c8cd697fa6f7d1ad9e750652549cb
jagodagardzielewska <122111893+jagodagardzielewska@users.noreply.github.com>
2024-03-14 20:36:14 +0100

    adding coach code view

---
+++ app/src/main/java/com/example/proindividual/AddingCoachCode.java
@@ -0,0 +1,14 @@
+package com.example.proindividual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingCoachCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_coach_code);
+    }
+}
